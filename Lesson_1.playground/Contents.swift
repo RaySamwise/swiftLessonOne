119e7706eb74ae83baaeb264e548c0c99d337311
Ray <Ray@Alekseis-MBP.Dlink>
2017-06-15 00:24:30 +0400

    test

---
+++ Lesson_1.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+//test
+let firstName = "Alex"
+let lastName = "Antipov"
+let height = "6'9\""
+let weight  = "220 lbs"
+let age = 28
+let maritalStatus = "Married"
+print("My name is \(firstName) \(lastName). I am \(age) years old. My height is \(height) and weight is \(weight). I am \(maritalStatus).")
+
+

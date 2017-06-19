6a92175345bc07d233212d28bec7704b3d8c2430
Ray <Ray@Alekseis-MBP.Dlink>
2017-06-19 23:37:34 +0400

    Lesson 2

--- Lesson_1.playground/Contents.swift
+++ Lesson_1.playground/Contents.swift
@@ -1,11 +1,30 @@
+let int8Min = Int8.min
+let int8Max = Int8.max
+let int16Min = Int16.min
+let int16Max = Int16.max
+let int32Min = Int32.min
+let int32Max = Int32.max
+let uInt8Min = UInt8.min
+let uInt8Max = UInt8.max
+let uInt16Min = UInt16.min
+let uInt16Max = UInt16.max
+let intMin = Int.min
+let intMax = Int.max
+let conInt = 1
+let conD = 1.8
+let conF : Float = 2.2
+let newConF : Float = Float(conD) + Float(conInt) + conF
+let newConD = Double(conF) + Double(conInt) + conD
+let newConI = Int(conD) + Int(conF) + conInt
+if newConI > Int(newConD)
+{
+    print("Integer \(newConI) greater than Double \(newConD)")
+}
+else
+{
+   print("Double \(newConD) greater than Integer \(newConI)")
+}
 
-//test comment
-let firstName = "Alex"
-let lastName = "Antipov"
-let height = "6'9\""
-let weight  = "220 lbs"
-let age = 28
-let maritalStatus = "Married"
-print("My name is \(firstName) \(lastName). I am \(age) years old. My height is \(height) and weight is \(weight). I am \(maritalStatus).")
+
 
 

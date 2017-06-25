d8db7d7e423fcf46ce447471390c7a3a2d71a3bb
Ray <Ray@Alekseis-MBP.Dlink>
2017-06-25 21:01:23 +0400

    1

---
+++ Lesson_3_Tuples.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: max push-ups, squads, pull-ups
+var myHomeExTuple = (pushups:30, squads:25, pullups:10)
+print(myHomeExTuple.0)
+print(myHomeExTuple.1)
+print(myHomeExTuple.pullups)
+var friendsHomeExTuple = (pushups:50, squads:55,pullups:25)
+
+//using new var's change the values of the tuples.

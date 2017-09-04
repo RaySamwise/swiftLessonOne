2a087f85a1cefc09348813fcf03e8ce824f03a52
Ray <Ray@Alekseis-MacBook-Pro.local>
2017-09-04 09:04:35 +0400

    JSON try2

---
+++ Course_app/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  Course_app
+//
+//  Created by Ray on 31/08/2017.
+//  Copyright © 2017 RayTo. All rights reserved.
+//
+
+import Foundation
+struct Advice {
+    let id: Int
+    let text: String
+    let state: Int
+    let sound: String
+
+}
+
---
+++ Course_app/Service.swift
@@ -0,0 +1,47 @@
+//
+//  Service.swift
+//  Course_app
+//
+//  Created by Ray on 31/08/2017.
+//  Copyright © 2017 RayTo. All rights reserved.
+//
+
+import Foundation
+class Service {
+    
+    var advices: [Advice]! = [Advice]()
+    
+    init() {
+        advices = loadAdvices(fileName: "advices", ext: "json")
+        
+    }
+    func loadAdvices(fileName : String, ext : String) -> [Advice]
+    {
+        let path =  Bundle.main.path(forResource: fileName, ofType: ext)
+        guard let resourcePath = path else  {
+            
+            return [Advice]()
+            
+        }
+        let url = URL(fileURLWithPath:resourcePath)
+        guard let data = try? Data(contentsOf: url)
+            else {
+                return [Advice]()
+                
+        }
+        //print(data)
+    
+        
+        
+        for advice in advices
+    {
+        try JSONSerialization.jsonObject(with: data) as? [[String:Any]]
+        print(advice)
+        }
+        return [Advice]()
+        
+        
+    }
+    
+    
+}
